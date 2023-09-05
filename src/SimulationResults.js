e56cca6f0e5aa2e97ab87c5dec918c1895501401
Anigirka <daniil.00.kov@gmail.com>
2023-09-05 15:44:56 +0700

    created abstraction for results of Simulation

---
+++ src/SimulationResults.js
@@ -0,0 +1,37 @@
+export default class SimulationResults {
+  constructor(txtResults) {
+    this._header = {}
+    this._columns = txtResults
+      .trim()
+      .split("\r\n")
+      .reduce((acc, item) => {
+        item.split("\t").forEach((el, index) => {
+          if (acc[index]) {
+            acc[index].push(Number(el.trim()))
+          } else {
+            this._header[el.trim()] = index
+            acc[index] = []
+          }
+        })
+        return acc
+      }, {})
+  }
+
+  getHeader() {
+    return this._header
+  }
+
+  getColByIndex(nCol) {
+    if (this._columns[nCol] === undefined) {
+      throw new Error("Not exist column with this index")
+    }
+    return this._columns[nCol]
+  }
+
+  getColByName(name) {
+    if (this._header[name] === undefined) {
+      throw new Error("Not exist column with this name")
+    }
+    return this._columns[this._header[name]]
+  }
+}
