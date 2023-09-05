5250aa1cbc850375790caca9a9ba7ac8d9468dff
Anigirka <daniil.00.kov@gmail.com>
2023-09-05 15:49:10 +0700

    created class line of simulation input data

---
+++ src/SimulationInput.js
@@ -0,0 +1,32 @@
+export default class SimulationInput {
+  constructor(precipitation, temperature, qobs) {
+    const _precipitation = Number(precipitation)
+    const _temperature = Number(temperature)
+    const _qobs = Number(qobs)
+
+    if (isNaN(_precipitation) || isNaN(_temperature) || isNaN(_qobs)) {
+      throw new Error(
+        "SimulationInput wrong values. Parameters can only be Number type"
+      )
+    }
+    this._data = {
+      date: new Date(),
+      precipitation: _precipitation,
+      temperature: _temperature,
+      qobs: _qobs
+    }
+  }
+
+  toString() {
+    const { date, precipitation, temperature, qobs } = this._data
+    const year = date.getFullYear().toString()
+    const month = (date.getMonth() + 1).toString().padStart(2, "0")
+    const _date = date.getDate().toString().padStart(2, "0")
+    const formatedData = year + month + _date
+    return [formatedData, precipitation, temperature, qobs].join("\t") + "\n"
+  }
+
+  show() {
+    console.log(this.toString())
+  }
+}
