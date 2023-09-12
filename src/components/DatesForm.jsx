a3ceff108f9adc87e63c570e6f78d07479d8b83e
danii <daniil.00.kov@gmail.com>
2023-09-12 18:34:55 +0700

    create data fetch in form

--- src/components/DatesForm.jsx
+++ src/components/DatesForm.jsx
@@ -1,14 +1,36 @@
 import DateInput from "./UI/DateInput/DateInput.jsx"
+
+function convertDate(dateFromInput) {
+  return dateFromInput.trim().split("-").join("")
+}
+
 const DatesForm = () => {
   function handleSubmit(e) {
     e.preventDefault()
-    console.log(e.target.startPeriod.value)
-    console.log(e.target.endPeriod.value)
+    const periodStart = convertDate(e.target.periodStart.value)
+    const periodEnd = convertDate(e.target.periodEnd.value)
+    console.log(periodStart, periodEnd)
+    fetch("/api/getCols", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        periodStart,
+        periodEnd,
+        columnsNames: ["Date"],
+        location: "Some"
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
   }
   return (
     <form onSubmit={handleSubmit}>
-      <DateInput text="Начало периода:" name="startPeriod" />
-      <DateInput text="Конец периода:" name="endPeriod" />
+      <DateInput text="Начало периода:" name="periodStart" />
+      <DateInput text="Конец периода:" name="periodEnd" />
       <button type="submit"> Показать </button>
     </form>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from "node:process"
+
+const PORT = process.env.PORT || 8080
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api" : `http://localhost:${PORT}`
+    }
+  }
 })
