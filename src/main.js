d544616fd96b624c76de644b9926492aab8142c9
danii <daniil.00.kov@gmail.com>
2023-10-15 12:06:25 +0700

    setting up the server for new logic

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import express, { json } from "express"
-import HBVSimulation from "./HBVSimulation.js"
 import process from "node:process"
 import { ValueRequireError } from "./ServerExeptions.js"
+import WeatherData from "./WeatherAPI/WeatherData.js"
+import UpdatableSimulation from "./UpdatableSimulation.js"
 
 const PORT = process.env.PORT || 8080
 
@@ -10,18 +11,34 @@ app.use(json())
 
 app.post("/api/getCols", (request, response) => {
   try {
-    const result = new HBVSimulation("./HBV-light_data/HBV-land", "test")
-      .readResultsInPeriod(request.body)
-      .getColumns(request.body)
+    const { periodStart, periodEnd, columnsNames, location } = request.body
+    const result = new UpdatableSimulation(
+      WeatherData.updatableLocations[location]
+    )
+      .readResultsInPeriod(new Date(periodStart), new Date(periodEnd))
+      .getColumns(columnsNames)
     response.send(result)
   } catch (err) {
     if (err instanceof ValueRequireError) {
       response.status("400")
       response.send(err.toJSON())
     } else {
+      console.error(err)
       response.status("500")
-      response.send()
+      response.send(err.message)
     }
   }
+})
+app.get("/api/locationsInfo", (req, res) => {
+  const data = WeatherData.locationsArray.map(el => {
+    const { key, name, lastInputArchDate, beginInputArchDate } = el
+    return {
+      key,
+      name,
+      lastDate: lastInputArchDate,
+      beginDate: beginInputArchDate
+    }
+  })
+  res.send(JSON.stringify(data))
 })
 app.listen(PORT)
