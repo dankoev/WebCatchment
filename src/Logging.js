ad36f7303812284a0a31d5f4aac4f326ad37a0b3
danii <daniil.00.kov@gmail.com>
2023-11-25 23:12:52 +0700

    added new information about location and logging

--- src/Logging.js
+++ src/Logging.js
@@ -1,7 +1,23 @@
+import * as fs from "fs"
+import path from "path"
+
 export default class Logging {
-  pathToLogFile = ".log."
-  static writeSuccess(message) { }
+  static pathToLogFile = path.resolve("./logs/logs.txt")
+
+  static writeSuccess(message) {
+    const now = new Date()
+    fs.appendFileSync(
+      this.pathToLogFile,
+      `${now.toISOString()}: #SUCCESS -> ${message} \n`
+    )
+  }
   static writeError(error) {
     console.log(`%c${error}`, "color: red;")
+    const now = new Date()
+    console.log(this.pathToLogFile)
+    fs.appendFileSync(
+      this.pathToLogFile,
+      `${now.toISOString()}: #ERROR ${error.message} \n`
+    )
   }
 }
